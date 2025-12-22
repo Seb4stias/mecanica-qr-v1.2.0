ec3c52861d5293fd3fc41d4ef4dd153761219d84
seb4stias <sebastiassalamanca@gmail.com>
2025-12-22 16:56:08 -0300

    arreglo caracteres corruptos , install connect-mongo, config mongo-store

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 // const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 require('dotenv').config();
@@ -25,6 +26,10 @@ app.use(session({
   secret: process.env.SESSION_SECRET || 'change-this-to-a-secure-random-string',
   resave: false,
   saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI,
+    touchAfter: 24 * 3600 // lazy session update
+  }),
   cookie: {
     secure: false, // Cambiar a true cuando tengas HTTPS configurado
     httpOnly: true,
